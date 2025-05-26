2343409683ba545f6c8c6a48ef9e1680c6f2dd78
Wallyson Siqueira <manin.mp40@gmail.com>
2025-05-26 15:25:27 -0400

    Add files via upload

--- lista_de_compras.py
+++ lista_de_compras.py
@@ -1,19 +1,34 @@
-compras = []
-opcao = input(print("Voce deseja adicionar mais algo ?"))
-while opcao=="sim":
-    adicionar=input("quais itens deseja adicionar ?apenas um por vez.")
-    compras.append(adicionar)
-    print(compras)
-    opcao=input("deseja continuar?")
+ # compras = []
+ # opcao = input(print("Voce deseja adicionar mais algo ?"))
+ # while opcao=="sim":
+ #     adicionar=input("quais itens deseja adicionar ?apenas um por vez.")
+ #     compras.append(adicionar)
+ #     print(compras)
+ #     opcao=input("deseja continuar?")
 
-organizar=input("deseja organisar a lista?")
+ # organizar=input("deseja organisar a lista?")
 
-if organizar=="sim":
-    compras_ordenadas=sorted(compras)
-    print(compras_ordenadas)
+ # if organizar=="sim":
+ #     compras_ordenadas=sorted(compras)
+ #     print(compras_ordenadas)
     
-    for item in compras:
-        print("-", item)
+ #     for item in compras:
+ #         print("-", item)
 
-elif organizar=="nao":
-    print(compras)
+ # elif organizar=="nao":
+ #  #     print(compras)
+
+# listas = []
+# dicionarios ={chave:valor}
+# tuplas= ()
+# conjuntos= set()
+
+# teste=(1,2,3,5)
+# print(teste)
+# teste.append(4)
+# print(teste)
+
+# teste=(1,2,3,5)
+# print(teste)
+# teste.remove(1)
+# print(teste)
